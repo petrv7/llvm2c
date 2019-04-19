45f34292235e1f6d1acb78f0e94604acbbe69da9
Petr Vitovský <xvitovs2@fi.muni.cz>
2019-04-19 22:27:00 +0200

    select test

---
+++ test/statements/select.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	if (argc != 2) {
+		return -1;
+	}
+
+	char *p;
+	long num = strtol(argv[1], &p, 10);
+
+	if (*p != '\0') {
+		return -1;
+	}
+
+	return num > 0 ? 1 : -1;
+}
